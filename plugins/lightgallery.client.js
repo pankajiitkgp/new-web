486ff6cf002d95b5342bda5bc6178d5c3620b5a8
Pankaj Sarkar <pankajmkd@gmail.com>
2022-01-19 16:14:36 +0530

    purgecss whitelist lightgalery.js

--- plugins/lightgallery.client.js
+++ plugins/lightgallery.client.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import lightgallery from 'lightgallery.js'
+/*! purgecss start ignore */
 import 'lightgallery.js/dist/css/lightgallery.min.css'
+/*! purgecss end ignore */
 // import 'lg-video.js'
 
 Vue.use(lightgallery)
